afb4b99cbf58bba5cd6525286698498d68e05310
yunkim <1106qwe@hanmail.net>
2023-10-17 19:34:25 +0900

    node - 실행 코드 부분 분리

--- node/node-app/nodeBasic2/app.js
+++ node/node-app/nodeBasic2/app.js
@@ -7,9 +7,6 @@ const app = express();
 //라우팅
 const home = require('./routes/home');
 
-//변수
-const POST = 3000;
-
 //app셋팅
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -18,6 +15,10 @@ app.set('view engine', 'ejs');
 routes / home / index.js에 정의한 api를 호출한다.*/
 app.use('/', home);
 
-app.listen(POST, ()=>{
-    console.log('server start');
-});
+module.exports = app;
+
+/*
+기존에 node 실행 코드
+nodemon app.js였으나 실행 부분을 분래해줘서 아래의 코드로 실행이 된다.
+nodemon ./bin/www.js
+*/
---
+++ node/node-app/nodeBasic2/bin/www.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const app = require('../app');
+
+//변수
+const PORT = 3000;
+
+app.listen(PORT, ()=> {
+    console.log('server start');
+});
