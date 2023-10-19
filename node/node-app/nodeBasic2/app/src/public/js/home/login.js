ac4ac605ea616ca483667d69fc939ea4dda1b2b0
yunkim <1106qwe@hanmail.net>
2023-10-19 14:16:42 +0900

    node - try catch 구문 구현

--- node/node-app/nodeBasic2/app/src/public/js/home/login.js
+++ node/node-app/nodeBasic2/app/src/public/js/home/login.js
@@ -20,6 +20,14 @@ function login() {
         body : JSON.stringify(req)//전송될 데이터
     })
     .then((res) => res.json())
-    .then((res) => console.log(res));
-    //.then((res) => console.log);//위와 동일
+    .then((res) => {
+        if(res.success){
+            location.href = '/';
+        }else{
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error('로그인 중 에러 발생'));
+    });
 }
