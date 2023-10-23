16d343db5e8b8be8fe4b3ae43e001cb1c6b10d90
yunkim <1106qwe@hanmail.net>
2023-10-23 14:25:57 +0900

    node - 데이터 파일로 읽기 (1)
    오류 발생 중 / 데이터 파일로 읽을 수 있게 변환 중

--- node/node-app/nodeBasic2/app/src/models/UserStorge.js
+++ node/node-app/nodeBasic2/app/src/models/UserStorge.js
@@ -1,15 +1,9 @@
 "use strict";
 
-/* 임시데이터 */
-class UserStorage{
-    /* static - 외부에서도 접속이 가능하다.
-    #를 통해서 은닉화해준다. (private 변수로 변경한다.)*/
-    static #users = {
-        id : ['test', 'root', 'yuni']
-        , pwd : ['test', 'root', '123']
-        , name : ['김아무개', '이아무개', '박아무개']
-    };
+const fs = require('fs');
 
+/* 임시데이터 */
+class UserStorage{   
     static getUsers(...fields){
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) =>{
@@ -22,23 +16,27 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
+        fs.readFile("./src/databases/users.json", (err, data) => {
+            if(err) throw err;
+            const users = JSON.parse(data);
 
-        /* 
-        keys - 키값만으로 배열을 만든다.
-        reduce - 순회하며 해당 idx에 해당되는 id, pwd, name이 []에 담긴다.
-        {} - 최종 값이 {}에 담긴다.
-        */
-        const userInfo = Object.keys(users).reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-            return newUsers;        
-        }, {});    
+            const idx = users.id.indexOf(id);
 
+            /* 
+            keys - 키값만으로 배열을 만든다.
+            reduce - 순회하며 해당 idx에 해당되는 id, pwd, name이 []에 담긴다.
+            {} - 최종 값이 {}에 담긴다.
+            */
+            const userInfo = Object.keys(users).reduce((newUsers, info) => {
+                newUsers[info] = users[info][idx];
+                return newUsers;        
+            }, {});   
+        });
         return userInfo;
     }
 
     static save(userInfo){
+        /*
         const users = this.#users;
 
         users.id.push(userInfo.id);
@@ -47,6 +45,7 @@ class UserStorage{
 
         console.log(users);
         return {success : true};
+        */
     }
 
 }
