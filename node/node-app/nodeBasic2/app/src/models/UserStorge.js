1c3e2f1cfa1ff0587ffbca5e4cb0c4e2f9083f7c
yunkim <1106qwe@hanmail.net>
2023-10-23 18:13:39 +0900

    node - db연결을 통한 구현(2)
    회원가입 부분 쿼리를 이용할 수 있도록 변경

--- node/node-app/nodeBasic2/app/src/models/UserStorge.js
+++ node/node-app/nodeBasic2/app/src/models/UserStorge.js
@@ -3,78 +3,25 @@
 const db = require('../config/db');
 
 /* 임시데이터 */
-class UserStorage{   
-    static #getUsers(data, isAll, fields){
-        const users = JSON.parse(data);
-
-        if(isAll) return users;
-
-        const newUsers = fields.reduce((newUsers, field) =>{
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});        
-        return newUsers;
-    }
-
-    static #getUserInfo(data, id){
-        const users = JSON.parse(data);
-        console.log(users)
-
-        const idx = users.id.indexOf(id);
-
-        /* 
-        keys - 키값만으로 배열을 만든다.
-        reduce - 순회하며 해당 idx에 해당되는 id, pwd, name이 []에 담긴다.
-        {} - 최종 값이 {}에 담긴다.
-        */
-        const userInfo = Object.keys(users).reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-            return newUsers;        
-        }, {});   
-
-        console.log(userInfo)
-        return userInfo;
-    }
-
-    static getUsers(isAll, ...fields){
-        return fs
-            .readFile("./src/databases/users.json")
-            .then((data) => {
-                return this.#getUsers(data, isAll, fields);
-            }).catch((err) => {
-                console.err
-            });
-    }
-
+class UserStorage{      
     static getUserInfo(id){
        return new Promise((resolve, reject) => {
-            db.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
-                if(err) reject(err);
+            const query = "SELECT * FROM users WHERE id = ?";
+            db.query(query, [id], (err, data) => {
+                if(err) reject(`${err}`);
                 resolve(data[0]);
             });
        });    
     }
 
     static async save(userInfo){
-        //const users = await this.getUsers("id", "password", "name");
-        const users = await this.getUsers(true);
-
-        //id가 존재하는지 확인
-        if(users.id.includes(userInfo.id)){
-            throw "이미 존재하는 아이디입니다.";
-        }else{            
-            users.id.push(userInfo.id);
-            users.name.push(userInfo.name);
-            users.pwd.push(userInfo.pwd);
-
-            fs.writeFile("./src/databases/users.json", JSON.stringify(users));
-            
-            return {success : true}
-        }
-
-        console.log(users)        
+        return new Promise((resolve, reject) => {
+            const query = "INSERT INTO users(id, name, pwd) VALUES(?, ?, ?)";
+            db.query(query, [userInfo.id, userInfo.name, userInfo.pwd], (err) => {
+                if(err) reject(`${err}`);
+                resolve({success : true});
+            });
+       });
     }
 
 }
