f1c6776748f852062c4bef764ba807983108ff06
yunkim <1106qwe@hanmail.net>
2023-10-24 13:29:46 +0900

    node - dotenv 환경변수 등록

--- node/node-app/nodeBasic2/app/src/config/db.js
+++ node/node-app/nodeBasic2/app/src/config/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host : "aws일 경우 엔드포인트"
-    , user : "아이디"
-    , password : "비밀번호"
-    , database : "테이블명"
+    host : process.env.DB_HOST
+    , user : process.env.DB_USER
+    , password : process.env.DB_PWD
+    , database : process.env.DB_DATABASE
 });
 
 //연결
