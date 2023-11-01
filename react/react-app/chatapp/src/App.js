534195410ad3f2faa6fd4d4be3ce706664546b66
yunkim <1106qwe@hanmail.net>
2023-11-01 13:40:50 +0900

    react - 유저정보 저장

--- react/react-app/chatapp/src/App.js
+++ react/react-app/chatapp/src/App.js
@@ -1,12 +1,37 @@
 /* eslint-disable */ 
+import { useEffect, useState } from 'react';
 import './App.css';
 import socket from './server';
 
 function App() {
+  useEffect(() => {
+    askUserName();
+  }, []);
+
+  //유저 정보를 저장하기 위한 state
+  const[user, setUser] = useState(null);
+
+  const askUserName = () => {   
+    const userName = prompt("당신의 이름을 입력하세요.")
+    console.log("user name ", userName) 
+
+    //로그인을 위해 유저이름과 콜백함수를 보낸다.
+    //emit처리가 되면 콜백함수로 응답을 받는다.
+    socket.emit("login", userName, (res) => {
+      console.log('Res ', res);
+
+      //if(res.ok){
+      if(res?.ok){
+        //유저정보를 저장
+        setUser(res.data);
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App"></div>
+    </>
   );
 }
 
--- react/react-app/chatapp/utils/io.js
+++ react/react-app/chatapp/utils/io.js
@@ -1,6 +1,23 @@
+const userController = require("../Controllers/user.controller")
+
 module.exports = function(io){
     io.on("connection ", async(socket)=>{
         console.log("client is connected ", socket.id);
+
+        /* App.js에서 socket.emit("login" 로 호출하면 아래에서 받는다. */
+        socket.on("login", async(userName, cb) => {
+            console.log("backend ", userName);
+
+            try {
+                //유저정보를 저장
+                const user = await userController.saveUser(userName, socket.id);
+                
+                //cb = 콜백 함수
+                cb({ok:true, data:user})
+            } catch (error) {
+                cb({ok:false, error:error.message})
+            }
+        })
 
         socket.io("disconnect", () => {
             console.log("user is disconnected");
