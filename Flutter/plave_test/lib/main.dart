4a00a8f80418a41e08d5a1d9742bd0987f5d4268
kim yunjung <1106qwe@hanmail.net>
2024-07-08 19:22:41 +0900

    상단 및 더미 자료

--- Flutter/plave_test/lib/main.dart
+++ Flutter/plave_test/lib/main.dart
@@ -6,7 +6,15 @@ import 'package:plave_test/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- Flutter/plave_test/lib/screen/home_screen.dart
+++ Flutter/plave_test/lib/screen/home_screen.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:plave_test/model/model_movie.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title' : '남예준',
+      'keyword' : '1등 신랑감/예덩이/도베르만두/레드아이즈',
+      'poster' : 'assets/images/yejun/plave_fancam.gif',
+      'like' : false
+    })
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text('real home'),),
+
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/plave.png', 
+            fit: BoxFit.contain, 
+            height: 25,
+          ),
+
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '멤버',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '멤버 간 케미',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 사진',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+
+      ),
+
     );
+
   }
+
 }
