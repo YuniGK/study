4095ca579e8582c9b6d20c0f6d10d4e758d63422
kim yunjung <1106qwe@hanmail.net>
2024-10-01 17:53:15 +0900

    -

--- js/react/basic-react/src/basic/Prop.js
+++ js/react/basic-react/src/basic/Prop.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 const Prop = (props) => {
-   
+   const [name, setName] = useState();
+
+    /*
+    아래와 같이 호출 할 경우, 페이지 로딩시 호출로 무한 반복 호출이 되어
+    오류가 발생한다.
+    
+    setName(props.name);
+    */
+
     return (
         <div>
             <p>{props.name}</p>
