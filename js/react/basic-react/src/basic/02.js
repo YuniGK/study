58a8f3bb93045129072010c36ebf9a79d93fd9bf
kim yunjung <1106qwe@hanmail.net>
2024-10-01 15:46:31 +0900

    sort

--- js/react/basic-react/src/basic/02.js
+++ js/react/basic-react/src/basic/02.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 
 const Basic02 = () => {
-    let [texts, setTexts] = useState(['1', '2', '3', '4', '5']);
+    //let [texts, setTexts] = useState(['1', '2', '3', '4', '5']);
+    let [texts, setTexts] = useState(['91', '12', '23', '34', '15']);
     let [num, setNum] = useState(0);
     let [num2, setNum2] = useState(0);
 
@@ -15,6 +16,12 @@ const Basic02 = () => {
         setTexts(newArray);
     }
 
+    function textSort(){
+        let newArray = [...texts];
+        newArray.sort();
+        setTexts(newArray);
+    }
+
     return (
         <div>
             <div className='black-nav'>
@@ -22,6 +29,7 @@ const Basic02 = () => {
             </div>
 
             <div className='list'>
+                <button onClick={textSort}>정렬</button>
                 <h3>{texts[0]} <span onClick={onClick}>{num}👍</span></h3>
                 <h3>{texts[1]} <span onClick={()=>{ setNum2(num2+1) }}>{num2}👍</span></h3>
                 <h3 onClick={textChange}>{texts[2]}</h3>
