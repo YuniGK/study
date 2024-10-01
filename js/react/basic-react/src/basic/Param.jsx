77571be88c06b8cc883813f5e765ef83454a7bca
kim yunjung <1106qwe@hanmail.net>
2024-10-01 18:19:00 +0900

    useParams

--- js/react/basic-react/src/basic/Param.jsx
+++ js/react/basic-react/src/basic/Param.jsx
@@ -1,4 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const Param = () => {
+    /* 
+    /param/:id 
+    /param/10으로 값이 넘어온 경우 
+    id = 10이라는 값을 읽을 수 있다. 
+    */
+    const {id} = useParams();
+
     return (
         <div className="param-page">
             <p>param</p>
